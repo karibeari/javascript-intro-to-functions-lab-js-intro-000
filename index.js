550a315b69a91ea2d8145f18cecf29256b559a68
Kari Ferenczy <klferenczy@gmail.com>
2019-01-25 17:10:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ var uppercase = "HELLO";
 var lowercase = "hello";
 
 function sayHiToGrandma(string){
-  if(string === lowercase){
+  if (string = "I love you, Grandma."){
+    return "I love you, too."
+  }
+  else if(string === lowercase){
     return "I can't hear you!"
   }
   else if(string === uppercase){
